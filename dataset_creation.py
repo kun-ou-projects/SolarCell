a6484e89b09b3d4a2b0405f296046f2464b64d7a
SatanuG <39297886+SatanuG@users.noreply.github.com>
2022-09-05 00:15:20 -0400

    changing absolute path to relative path

--- dataset_creation.py
+++ dataset_creation.py
@@ -8,25 +8,16 @@ cwt = ChemWordTokenizer()
 
 
 
-db_file = "C:/Users/satan/PycharmProjects/SolarCell/data/abstracts.db"
+db_file = "data/abstracts.db"
 
 con = sqlite3.connect(db_file)
-##abstracts with “band gap” (e.g. “band-gap”, “bandgap”, “band gaps”, “band-gaps”, “bandgaps”), with explicit mentions of “eV”
-#df = pd.read_sql_query("Select * from tblAbstract where (abstract like '%band gap%' OR abstract like '%bandgap%' OR abstract like '%band gaps%' OR abstract like '%band-gaps%' OR abstract like '%bandgaps%') AND"
-#                       " (abstract like '%eV%' OR abstract like '%electron volt%' OR abstract like '%e-V')", con)
-#df = pd.read_sql_query("Select * from tblAbstract where abstract like '%band gap%' OR abstract like '%bandgap%' OR abstract like '%band gaps%' OR abstract like '%band-gaps%' OR abstract like '%bandgaps%' AND abstract like '%eV%' OR abstract like '%electron volt%' OR abstract like '%e-V%'", con)
-
-#sql_command = "Select * from tblAbstract where abstract like ('%band gap%' AND '%eV%') OR abstract like ('%bandgap%' AND '%eV%') OR abstract like ('%band gaps%' AND '%eV%') OR abstract like ('%band-gaps%' AND '%eV%') OR abstract like ('%bandgaps%' AND '%eV%')"
 sql_command = "Select * from tblAbstract where abstract like '%band gap%' OR abstract like '%bandgap%' OR abstract like '%band-gap%' OR abstract like '%band gaps%' OR abstract like '%band-gaps%' OR abstract like '%bandgaps%' " \
               "OR title like '%band gap%' OR title like '%bandgap%' OR title like '%band-gap%' OR title like '%band gaps%' OR title like '%band-gaps%' OR title like '%bandgaps%'"
 df = pd.read_sql_query(sql_command, con)
 
-#abst = df['abstract']
-#print(abst)
 print(df.columns)
 
 
-#df.to_csv("C:/Users/satan/PycharmProjects/SolarCell/data/evaluation_set.csv")
 conditioned_dataset = pd.DataFrame()
 for index, rows in df.iterrows():
     #print(type(index))
@@ -39,9 +30,8 @@ for index, rows in df.iterrows():
 
 
 
-#print(len(conditioned_dataset))
 print("Dataset reduced from %d to %d" %(len(df), len(conditioned_dataset)))
 
 manual_evaluation = conditioned_dataset.sample(n=500, random_state=22)
-manual_evaluation.to_csv("C:/Users/satan/PycharmProjects/SolarCell/data/manual_evaluation_dataset.csv")
-conditioned_dataset.to_csv("C:/Users/satan/PycharmProjects/SolarCell/data/relevant_dataset.csv")
+manual_evaluation.to_csv("data/manual_evaluation_dataset.csv")
+conditioned_dataset.to_csv("data/relevant_dataset.csv")
